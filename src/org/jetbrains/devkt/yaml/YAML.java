fe33fe1e8841e773b3a56869ae5bb1f26b12a30e
ice1000 <ice1000@sbrella.com>
2018-04-28 19:25:25 +0800

    Add highlight

--- src/org/jetbrains/devkt/yaml/YAML.java
+++ src/org/jetbrains/devkt/yaml/YAML.java
@@ -1,7 +1,10 @@
 package org.jetbrains.devkt.yaml;
 
+import org.ice1000.devkt.openapi.ColorScheme;
 import org.ice1000.devkt.openapi.ExtendedDevKtLanguage;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.com.intellij.psi.tree.IElementType;
 
 import javax.swing.*;
 
@@ -13,9 +16,18 @@ public class YAML<T> extends ExtendedDevKtLanguage<T> {
 		super(YAMLLanguage.INSTANCE, new YAMLParserDefinition());
 	}
 
-	@NotNull
+	@Nullable
 	@Override
-	public Icon getIcon() {
+	public T attributesOf(@NotNull IElementType type, @NotNull ColorScheme<? extends T> colorScheme) {
+		if (type == YAMLTokenTypes.COMMA) return colorScheme.getComma();
+		else if (type == YAMLTokenTypes.COLON) return colorScheme.getColon();
+		else if (type == YAMLTokenTypes.COMMENT) return colorScheme.getLineComments();
+		else if (type == YAMLTokenTypes.SCALAR_KEY) return colorScheme.getKeywords();
+		else return super.attributesOf(type, colorScheme);
+	}
+
+	@Override
+	public @NotNull Icon getIcon() {
 		return YAMLIcons.FILE;
 	}
 }
